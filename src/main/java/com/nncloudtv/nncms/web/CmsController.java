b47d001b896760e87c8ffa646428ca7ca403f8b0
louis <louis@a0a5f996-2bb4-401c-8778-395c3c01700f>
2012-09-12 11:22:49 +0000

    html view
    git-svn-id: https://everest.teltel.com:8443/repos/misc/9x9dev/trunk/nncms@3274 a0a5f996-2bb4-401c-8778-395c3c01700f

--- src/main/java/com/nncloudtv/nncms/web/CmsController.java
+++ src/main/java/com/nncloudtv/nncms/web/CmsController.java
@@ -2,11 +2,21 @@ package com.nncloudtv.nncms.web;
 
 import java.util.logging.Logger;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CmsController {
 	
 	protected static final Logger log = Logger.getLogger(CmsController.class.getName());
+	
+	@RequestMapping("{html}.html")
+	public String html(HttpServletRequest req, HttpServletResponse resp, @PathVariable String html) {
+	    return html + ".html";
+	}
 	
 }
